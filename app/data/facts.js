1ed1d9290e46515b46dd262601626b18bdd0e71f
Sylvain METAYER <contact@sylvainmetayer.fr>
2018-04-09 10:50:59 +0200

    Add facts rules #7

--- app/data/facts.js
+++ app/data/facts.js
@@ -1,5 +1,26 @@
 // TODO Ajouter les faits ici
 
 module.exports = {
-  todo: 'Here are all facts'
+  '3_COTES': 'a trois cotes',
+  '2_COTES_EGAUX': 'a deux cotes egaux',
+  '3_COTES_EGAUX': 'a trois cotes egaux',
+  '1_ANGLE_DROIT': 'a un angle droit',
+  'TRIANGLE': 'triangle',
+  'TRIANGLE_RECTANGLE': 'triangle rectangle',
+  'TRIANGLE_ISOCELE_RECTANGLE': 'triangle isocele rectangle',
+  'TRIANGLE_ISOCELE': 'triangle isocele',
+  'TRIANGLE_EQUILATERAL': 'triangle equilateral',
+  '4_COTES': 'a quatres cotes',
+  '4_COTES_EGAUX': 'a quatres cotes egaux',
+  '2_COTES_OPPOSES_EGAUX': 'a deux cotes opposes egaux',
+  '2_COTES_OPPOSES_PARALLELES': 'a deux cotes opposes paralleles',
+  '2_ANGLES_DROITS': 'a deux angles droits',
+  '4_ANGLES_DROITS': 'a quatres angles droits',
+  '3_ANGLES_DROITS': 'a trois angles droits',
+  'QUADRILATERE': 'quadrilatere',
+  'LOSANGE': 'Losange',
+  'CARRE': 'carre',
+  'TRAPEZE': 'trapeze',
+  'RECTANGLE': 'rectangle',
+  'QUADRILATERE_DEUX_COTES_OPPOSES_EGAUX': 'quadrilatere a deux cotes opposes egaux'
 }
